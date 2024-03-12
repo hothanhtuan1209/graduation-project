c4535870f58e53bd53ca96a7b90fc9a6ee6f9f3a
Tuan Ho <hothanhtuan1209@gmai.com>
2024-03-12 16:15:18 +0700

    Create User model and database

--- property_web/users/models.py
+++ property_web/users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.core.validators import RegexValidator
 
-# Create your models here.
+from property_web.models import BaseModel
+
+
+class User(AbstractBaseUser, BaseModel):
+    """
+    A custom user model with phone number field.
+
+    This model is used to represent users in the system. It inherits from
+    Django's built-in AbstractBaseUser and also includes some additional fields
+    from the BaseModel.
+
+    Attributes:
+        phone_number (str): The phone number of the user. Must be a 10-digit
+            number starting with '0'. This field is unique across all users.
+    """
+
+    phone_number = models.CharField(
+        validators=[RegexValidator(r"^0\d{9}$")], max_length=10, unique=True
+    )
+
+    def __str__(self):
+        """
+        Return a string representation of the User object.
+        """
+
+        return str(self.username)
