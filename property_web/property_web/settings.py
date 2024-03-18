5b08a9ce1501afc4b12c168a76248ffde485f158
Tuan Ho <hothanhtuan1209@gmail.com>
2024-03-18 11:11:41 +0700

    Fix issue, add authentication to installed_app

--- property_web/property_web/settings.py
+++ property_web/property_web/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "authentication",
     "users",
     "category",
     "posts",
@@ -62,9 +62,7 @@ ROOT_URLCONF = "property_web.urls"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'authentication/templates')
-        ],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
