1c2057f56f4631c623f35575721042f3efeff3a5
Tuan Ho <hothanhtuan1209@gmail.com>
2024-03-18 10:36:40 +0700

    Update url for login feature

--- property_web/authentication/urls.py
+++ property_web/authentication/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import user_signup, user_login
 
 urlpatterns = [
-    path('', user_login, name='login'),
+    path('login/', user_login, name='login'),
     path('signup/', user_signup, name='signup')
 ]
