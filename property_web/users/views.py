04bef7c3df60141c1bf7ce27cf181e268fba2c1d
Tuan Ho <hothanhtuan1209@gmail.com>
2024-03-18 09:59:23 +0700

    Implement get user detail and eidt information user

--- property_web/users/views.py
+++ property_web/users/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_http_methods
+from django.urls import reverse
 
 from .models import CustomUser
+from .forms import UserForm
 
 
 @require_http_methods(["GET"])
@@ -13,3 +15,21 @@ def user_detail(request, user_id):
     user = get_object_or_404(CustomUser, id=user_id)
     context = {'user': user}
     return render(request, 'detail.html', context)
+
+
+@require_http_methods(["GET", "POST"])
+def update_user(request, user_id):
+    """
+    This function to update information of employee.
+    """
+
+    user = get_object_or_404(CustomUser, id=user_id)
+
+    form = UserForm(request.POST or None, instance=user)
+
+    if form.is_valid():
+        user = form.save()
+        detail_url = reverse('user_detail', args=[str(user.id)])
+        return redirect(detail_url)
+
+    return render(request, 'update.html', {'form': form})
