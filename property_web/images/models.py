b96465e2c9a41028b5207b6952e79051d51ad93d
Tuan Ho <hothanhtuan1209@gmail.com>
2024-03-30 20:59:48 +0700

    Implement feature create post and upload file image

--- property_web/images/models.py
+++ property_web/images/models.py
@@ -1,15 +1,8 @@
 from django.db import models
-from storages.backends.s3boto3 import S3Boto3Storage
-
-from property_web.models import BaseModel
 from posts.models import Post
 
 
-class S3MediaStorage(S3Boto3Storage):
-    location = 'media'
-
-
-class Image(BaseModel):
+class Image(models.Model):
     """
     This model represents an image uploaded within the system.
 
@@ -19,5 +12,8 @@ class Image(BaseModel):
         post this image belongs to.
     """
 
-    image = models.ImageField(storage=S3MediaStorage())
+    image = models.ImageField(upload_to='images/')
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.image)
--- property_web/posts/views.py
+++ property_web/posts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm, ImageForm
+from .models import Post
+from images.models import Image
 
-# Create your views here.
+
+def create_post(request):
+    """
+    This view is for creating a new post feature.
+    """
+
+    post_form = PostForm(request.POST or None)
+    image_form = ImageForm(request.POST or None, request.FILES or None)
+
+    if request.method == "POST" and post_form.is_valid() and image_form.is_valid():
+        post = post_form.save(commit=False)
+        post.user = request.user
+        post.save()
+
+        image = image_form.cleaned_data.get("image")
+        if image:
+            Image.objects.create(post=post, image=image)
+
+        return redirect("home")
+
+    context = {"post_form": post_form, "image_form": image_form}
+    return render(request, "create_post.html", context)
