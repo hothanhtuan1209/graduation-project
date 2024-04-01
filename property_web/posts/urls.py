232f93f88e8cbeeba618d5213544c5acb333fa82
Tuan Ho <hothanhtuan1209@gmail.com>
2024-04-01 23:56:53 +0700

    Implement get list post feature and create home page

--- property_web/posts/urls.py
+++ property_web/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import create_post
+from .views import create_post, post_list
 
 urlpatterns = [
     path('create/', create_post, name='create_post'),
+    path('', post_list, name='list_post')
 ]
--- property_web/posts/views.py
+++ property_web/posts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from property_web.constants.enum import Status
 from .forms import PostForm, ImageForm
 from .models import Post
 from images.models import Image
@@ -21,7 +24,23 @@ def create_post(request):
         if image:
             Image.objects.create(post=post, image=image)
 
-        return redirect("home")
+        return redirect("list_post")
 
     context = {"post_form": post_form, "image_form": image_form}
     return render(request, "create_post.html", context)
+
+
+def post_list(request):
+    """
+    View function to display a list of posts categorized as hot and normal.
+    """
+
+    hot_posts = Post.objects.filter(
+        hot_post=True, status=Status.AVAILABLE
+    ).order_by("-created_at")
+    normal_posts = Post.objects.filter(
+        hot_post=False, status=Status.AVAILABLE
+    ).order_by("-created_at")
+
+    context = {"hot_posts": hot_posts, "normal_posts": normal_posts}
+    return render(request, "list_posts.html", context)
