60778f3b9da3ff052ac8ea0676114288207c1579
Tuan Ho <hothanhtuan1209@gmail.com>
2024-04-10 14:52:38 +0700

    Use dict for query filter

--- property_web/posts/helpers/build_query_filter.py
+++ property_web/posts/helpers/build_query_filter.py
@@ -8,6 +8,20 @@ def build_query_filter(request_data):
 
     query_filter = Q()
 
+    # Mapping for price range
+    price_dict = {
+        'lt_2m': Q(price__lt=2000000),
+        '2m_to_3m': Q(price__range=(2000000, 3000000)),
+        'gt_3m': Q(price__gt=3000000)
+    }
+
+    # Mapping for area range
+    area_dict = {
+        'lt_15': Q(area__lt=15),
+        '15_to_25': Q(area__range=(15, 25)),
+        'gt_25': Q(area__gt=25)
+    }
+
     # Filter by address
     address = request_data.get('address')
     if address:
@@ -16,21 +30,15 @@ def build_query_filter(request_data):
     # Filter by price
     price_range = request_data.get('price_range')
     if price_range:
-        if price_range == 'lt_2m':
-            query_filter &= Q(price__lt=2000000)
-        elif price_range == '2m_to_3m':
-            query_filter &= Q(price__range=(2000000, 3000000))
-        elif price_range == 'gt_3m':
-            query_filter &= Q(price__gt=3000000)
+        price_filter = price_dict.get(price_range)
+        if price_filter:
+            query_filter &= price_filter
 
     # Filter by area
     area_range = request_data.get('area_range')
     if area_range:
-        if area_range == 'lt_15':
-            query_filter &= Q(area__lt=15)
-        elif area_range == '15_to_25':
-            query_filter &= Q(area__range=(15, 25))
-        elif area_range == 'gt_25':
-            query_filter &= Q(area__gt=25)
+        area_filter = area_dict.get(area_range)
+        if area_filter:
+            query_filter &= area_filter
 
     return query_filter
