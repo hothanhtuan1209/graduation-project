84abffe49bb5f412bfb2fd4d31541bb1298d60c7
Tuan Ho <hothanhtuan1209@gmail.com>
2024-04-10 15:05:16 +0700

    Remove home page in authentication app

--- property_web/authentication/urls.py
+++ property_web/authentication/urls.py
@@ -4,11 +4,9 @@ from .views import (
     user_login,
     change_password,
     user_logout,
-    home_page
     )
 
 urlpatterns = [
-    path('', home_page, name='home'),
     path('login/', user_login, name='login'),
     path('signup/', user_signup, name='signup'),
     path('changepassword/', change_password, name='change_password'),
--- property_web/authentication/views.py
+++ property_web/authentication/views.py
@@ -64,31 +64,6 @@ def user_login(request):
     return render(request, "login.html", {"form": form})
 
 
-@csrf_exempt
-def home_page(request):
-    """
-    View function to display a list of posts categorized as hot and normal.
-    """
-
-    posts = (
-        Post.objects.filter(status=Status.AVAILABLE.value)
-        .values("id", "title", "price", "address", "hot_post")
-        .order_by("-created_at")
-    )
-
-    post_ids = [post["id"] for post in posts]
-    images = Image.objects.filter(post_id__in=post_ids)
-    image_mapping = {image.post_id: image.image.url for image in images}
-
-    for post in posts:
-        post["image"] = image_mapping.get(post["id"])
-
-    hot_posts = [post for post in posts if post["hot_post"]][:10]
-    normal_posts = [post for post in posts if not post["hot_post"]][:10]
-    context = {"hot_posts": hot_posts, "normal_posts": normal_posts}
-    return render(request, "home.html", context)
-
-
 @login_required
 def change_password(request):
     """
