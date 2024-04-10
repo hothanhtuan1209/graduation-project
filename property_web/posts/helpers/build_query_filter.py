4dec8468596559eb3ece88ded0f25c56b7bb237e
Tuan Ho <hothanhtuan1209@gmail.com>
2024-04-10 15:34:56 +0700

    Move create dict out function filter

--- property_web/posts/helpers/build_query_filter.py
+++ property_web/posts/helpers/build_query_filter.py
@@ -1,5 +1,19 @@
 from django.db.models import Q
 
+# Define mapping for price range
+price_dict = {
+    'lt_2m': Q(price__lt=2000000),
+    '2m_to_3m': Q(price__range=(2000000, 3000000)),
+    'gt_3m': Q(price__gt=3000000)
+}
+
+# Define mapping for area range
+area_dict = {
+    'lt_15': Q(area__lt=15),
+    '15_to_25': Q(area__range=(15, 25)),
+    'gt_25': Q(area__gt=25)
+}
+
 
 def build_query_filter(request_data):
     """
@@ -7,20 +21,6 @@ def build_query_filter(request_data):
     """
 
     query_filter = Q()
-
-    # Mapping for price range
-    price_dict = {
-        'lt_2m': Q(price__lt=2000000),
-        '2m_to_3m': Q(price__range=(2000000, 3000000)),
-        'gt_3m': Q(price__gt=3000000)
-    }
-
-    # Mapping for area range
-    area_dict = {
-        'lt_15': Q(area__lt=15),
-        '15_to_25': Q(area__range=(15, 25)),
-        'gt_25': Q(area__gt=25)
-    }
 
     # Filter by address
     if address := request_data.get('address'):
