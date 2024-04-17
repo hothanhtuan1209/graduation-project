b1e147242e5cf5d77419672cdbb98d759405b609
Tuan Ho <hothanhtuan1209@gmail.com>
2024-04-18 01:19:26 +0700

    Update TemplateView for views post

--- property_web/posts/views.py
+++ property_web/posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic import TemplateView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_http_methods
 from django.urls import reverse
@@ -15,155 +16,125 @@ from .helpers.build_query_filter import build_query_filter
 from property_web.settings import PAGE_SIZE
 
 
-@login_required
-def create(request):
-    """
-    This view is for creating a new post feature.
-    """
+class CreatePostView(TemplateView):
+    template_name = 'create_post.html'
 
-    if request.method == "POST":
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_form'] = PostForm()
+        context['image_form'] = ImageForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
         post_form = PostForm(request.POST)
         image_form = ImageForm(request.POST, request.FILES)
-
         if post_form.is_valid() and image_form.is_valid():
             post = post_form.save(commit=False)
             post.user = request.user
             post.save()
-
             images = request.FILES.getlist("image")
-            image_instances = [Image(post=post, image=image) for image in images]
-            Image.objects.bulk_create(image_instances)
-
+            for image in images:
+                Image.objects.create(post=post, image=image)
             user_id = request.user.id
             return redirect(reverse('user_detail', kwargs={'user_id': user_id}))
-    else:
-        post_form = PostForm()
-        image_form = ImageForm()
-
-    context = {"post_form": post_form, "image_form": image_form}
-    return render(request, "create_post.html", context)
-
-
-def list_post(request):
-    """
-    This function to get list all posts have status is AVAILABLE, user can
-    filter, search data
-    """
-
-    request_data = request.GET
-    query_filter = build_query_filter(request_data)
-    order_by = request_data.get("order_by", "-created_at")
-
-    posts = (
-        Post.objects.filter(Q(status=Status.AVAILABLE.value) & query_filter)
-        .order_by(order_by)
-        .values("id", "title", "price", "address", "area")
-    )
-
-    order_by_selected = request.GET.get('order_by')
-
-    paginator = Paginator(posts, PAGE_SIZE)
-    page = request.GET.get("page", 1)
-
-    try:
-        posts_page = paginator.page(page)
-    except (PageNotAnInteger, EmptyPage):
-        posts_page = paginator.page(1)
-
-    post_ids = [post["id"] for post in posts_page]
-    images = Image.objects.filter(post_id__in=post_ids)
-    image_mapping = {image.post_id: image.image.url for image in images}
-
-    for post in posts_page:
-        post["image"] = image_mapping.get(post["id"])
-
-    context = {
-        "posts": posts_page,
-        "order_by_selected": order_by_selected
-    }
-
-    return render(request, "list_posts.html", context)
+        else:
+            context = self.get_context_data(**kwargs)
+            context['post_form'] = post_form
+            context['image_form'] = image_form
+            return self.render_to_response(context)
 
 
-def post_detail(request, post_id):
-    """
-    View function to get detail post by post ID. Addition get
-    information user, list post by user ID
-    """
+class ListPostView(TemplateView):
+    template_name = 'list_posts.html'
 
-    post = get_object_or_404(Post, pk=post_id)
-    post_images = post.image_set.all()
-
-    user_post = (
-        CustomUser.objects.filter(pk=post.user_id)
-        .values("username", "phone_number", "id")
-        .get()
-    )
-
-    posts_of_user = (
-        Post.objects.filter(Q(status=Status.AVAILABLE.value) & Q(user=user_post["id"]))
-        .exclude(id=post_id)
-        .order_by("-created_at")
-        .values("id", "title", "address")[:5]
-    )
-
-    post_ids = [post["id"] for post in posts_of_user]
-    images = Image.objects.filter(post_id__in=post_ids)
-    image_mapping = {image.post_id: image.image.url for image in images}
-
-    for post_user in posts_of_user:
-        post_user["image"] = image_mapping.get(post_user["id"])
-
-    context = {
-        "post": post,
-        "post_images": post_images,
-        "user_post": user_post,
-        "posts_of_user": posts_of_user,
-    }
-
-    return render(request, "post_detail.html", context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        request_data = self.request.GET
+        query_filter = build_query_filter(request_data)
+        order_by = request_data.get("order_by", "-created_at")
+        posts = (
+            Post.objects.filter(Q(status=Status.AVAILABLE.value) & query_filter)
+            .order_by(order_by)
+            .values("id", "title", "price", "address", "area")
+        )
+        paginator = Paginator(posts, PAGE_SIZE)
+        page = self.request.GET.get("page", 1)
+        try:
+            posts_page = paginator.page(page)
+        except (PageNotAnInteger, EmptyPage):
+            posts_page = paginator.page(1)
+        post_ids = [post["id"] for post in posts_page]
+        images = Image.objects.filter(post_id__in=post_ids)
+        image_mapping = {image.post_id: image.image.url for image in images}
+        for post in posts_page:
+            post["image"] = image_mapping.get(post["id"])
+        context["posts"] = posts_page
+        context["order_by_selected"] = self.request.GET.get('order_by')
+        return context
 
 
-@login_required
-@require_http_methods(["GET", "POST"])
-def update_post(request, post_id):
-    """
-    This function to get instance of post and display in form. Allows user
-    to edit it
-    """
+class PostDetailView(TemplateView):
+    template_name = 'post_detail.html'
 
-    post = get_object_or_404(Post, id=post_id)
-    images_urls = [image.image.url for image in Image.objects.filter(post=post)]
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=post_id)
+        post_images = post.image_set.all()
+        user_post = (
+            CustomUser.objects.filter(pk=post.user_id)
+            .values("username", "phone_number", "id")
+            .get()
+        )
+        posts_of_user = (
+            Post.objects.filter(Q(status=Status.AVAILABLE.value) & Q(user=user_post["id"]))
+            .exclude(id=post_id)
+            .order_by("-created_at")
+            .values("id", "title", "address")[:5]
+        )
+        post_ids = [post["id"] for post in posts_of_user]
+        images = Image.objects.filter(post_id__in=post_ids)
+        image_mapping = {image.post_id: image.image.url for image in images}
+        for post_user in posts_of_user:
+            post_user["image"] = image_mapping.get(post_user["id"])
+        context["post"] = post
+        context["post_images"] = post_images
+        context["user_post"] = user_post
+        context["posts_of_user"] = posts_of_user
+        return context
 
-    if request.method == "POST":
+
+class UpdatePostView(TemplateView):
+    template_name = 'edit_post.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        images_urls = [image.image.url for image in Image.objects.filter(post=post)]
+        context["post_form"] = PostForm(instance=post)
+        context["image_form"] = ImageForm()
+        context["images_urls"] = images_urls
+        return context
+
+    def post(self, request, *args, **kwargs):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
         post_form = PostForm(request.POST, instance=post)
         image_form = ImageForm(request.POST, request.FILES, instance=post)
-
         if post_form.is_valid() and image_form.is_valid():
             post = post_form.save()
-
             images = request.FILES.getlist("image")
-            image_instances = [Image(post=post, image=image) for image in images]
-            Image.objects.bulk_create(image_instances)
-
+            for image in images:
+                Image.objects.create(post=post, image=image)
             return redirect(reverse("post_detail", kwargs={"post_id": post.id}))
-
-    else:
-        post_form = PostForm(instance=post)
-        image_form = ImageForm()
-
-    return render(
-        request,
-        "edit_post.html",
-        {
-            "post_form": post_form,
-            "image_form": image_form,
-            "images_urls": images_urls,
-        },
-    )
+        else:
+            context = self.get_context_data(**kwargs)
+            return self.render_to_response(context)
 
 
 @login_required
+@require_http_methods(["POST"])
 def delete_post(request, post_id):
     """
     View function to delete a post.
