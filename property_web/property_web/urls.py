0f7cdfdfad72e73e65bcd98c20d279232a4bef70
Tuan Ho <hothanhtuan1209@gmail.com>
2024-04-18 00:22:51 +0700

    Update templateview for home page

--- property_web/property_web/urls.py
+++ property_web/property_web/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home_page
+from .views import HomePageView
 
 urlpatterns = [
-    path('home', home_page, name='home'),
+    path('home', HomePageView.as_view(), name='home'),
     path('user/', include('users.urls')),
     path('auth/', include('authentication.urls')),
     path("admin/", admin.site.urls),
--- property_web/property_web/views.py
+++ property_web/property_web/views.py
@@ -1,31 +1,32 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
 
 from posts.models import Post
 from images.models import Image
 from property_web.constants.enum import Status
 
 
-@csrf_exempt
-def home_page(request):
-    """
-    View function to display a list of posts categorized as hot and normal.
-    """
+class HomePageView(TemplateView):
+    template_name = "home.html"
 
-    posts = (
-        Post.objects.filter(status=Status.AVAILABLE.value)
-        .values("id", "title", "price", "address", "hot_post")
-        .order_by("-created_at")
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts = (
+            Post.objects.filter(status=Status.AVAILABLE.value)
+            .values("id", "title", "price", "address", "hot_post")
+            .order_by("-created_at")
+        )
 
-    post_ids = [post["id"] for post in posts]
-    images = Image.objects.filter(post_id__in=post_ids)
-    image_mapping = {image.post_id: image.image.url for image in images}
+        post_ids = [post["id"] for post in posts]
+        images = Image.objects.filter(post_id__in=post_ids)
+        image_mapping = {image.post_id: image.image.url for image in images}
 
-    for post in posts:
-        post["image"] = image_mapping.get(post["id"])
+        for post in posts:
+            post["image"] = image_mapping.get(post["id"])
 
-    hot_posts = [post for post in posts if post["hot_post"]][:10]
-    normal_posts = [post for post in posts if not post["hot_post"]][:10]
-    context = {"hot_posts": hot_posts, "normal_posts": normal_posts}
-    return render(request, "home.html", context)
+        hot_posts = [post for post in posts if post["hot_post"]][:10]
+        normal_posts = [post for post in posts if not post["hot_post"]][:10]
+
+        context["hot_posts"] = hot_posts
+        context["normal_posts"] = normal_posts
+
+        return context
