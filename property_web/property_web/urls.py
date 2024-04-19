c5e1f75812f8251f47e01dd17fae31805f95557c
Tuan Ho <hothanhtuan1209@gmail.com>
2024-04-19 16:56:01 +0700

    Update home page default when runserver

--- property_web/property_web/urls.py
+++ property_web/property_web/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import RedirectView
 from django.conf.urls.static import static
 from .views import HomePageView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/home', permanent=False)),
     path('home', HomePageView.as_view(), name='home'),
     path('user/', include('users.urls')),
     path('auth/', include('authentication.urls')),
