bd6ac676baaef39d92f52a303d614759c0a8a3ed
Tuan Ho <hothanhtuan1209@gmail.com>
2024-05-03 10:41:26 +0700

    Translate comments code into English

--- property_web/posts/static/js/address_detail.js
+++ property_web/posts/static/js/address_detail.js
@@ -3,8 +3,8 @@ document.addEventListener(
   function () {
     const addressElement = document.getElementById("address-output");
 
-    if (addressElement) { // Kiểm tra nếu phần tử và dữ liệu tồn tại
-      addressElement.innerHTML = formatAddress(addressElement.innerHTML);; // Sử dụng innerHTML để hiển thị HTML
+    if (addressElement) { // Check if element and data exist
+      addressElement.innerHTML = formatAddress(addressElement.innerHTML);; // Use innerHTML to display HTML
       addressElement.style.display = 'flex';
     } else {
       console.error("Dữ liệu địa chỉ không tồn tại hoặc phần tử không được tìm thấy.");
@@ -15,7 +15,7 @@ document.addEventListener(
 
 function formatAddress(addressString) {
   if (!addressString) {
-    return ""; // Xử lý địa chỉ trống
+    return ""; //Handling empty addresses
   }
 
   try {
@@ -28,18 +28,18 @@ function formatAddress(addressString) {
     if (address.streetName) {
       formattedAddress += address.streetName + ", ";
     }
-    if (address.ward) { // Sử dụng address.ward thay vì obj.ward
+    if (address.ward) { // Use address.ward instead of obj.ward
       formattedAddress += address.ward.name + ", ";
     }
-    if (address.district) { // Sử dụng address.district
+    if (address.district) { // Use address.district
       formattedAddress += address.district.name + ", ";
     }
-    if (address.city) { // Sử dụng address.city
+    if (address.city) { // Use address.city
       formattedAddress += address.city.name;
     }
-    return formattedAddress; // Loại bỏ dấu phẩy và khoảng trắng ở cuối
+    return formattedAddress; // Remove trailing commas and spaces
   } catch (error) {
     console.error("Lỗi phân tích JSON địa chỉ:", error);
-    return ""; // Xử lý lỗi phân tích
+    return ""; // Handle parsing errors
   }
 }
