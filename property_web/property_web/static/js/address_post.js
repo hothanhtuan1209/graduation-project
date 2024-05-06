b3d92410b761690520b457a084d0d1f5cf6bf861
Tuan Ho <hothanhtuan1209@gmail.com>
2024-05-06 10:56:38 +0700

    Remove comment VietNamese

--- property_web/property_web/static/js/address_post.js
+++ property_web/property_web/static/js/address_post.js
@@ -1,49 +1,49 @@
 document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    const addressElements = document.getElementsByClassName("address-content");
-    console.log("here: ", addressElements.length);
-    if (addressElements.length > 0) {
-      for (let index = 0; index < addressElements.length; index++) {
-        const element = addressElements[index];
-        element.innerHTML = formatAddress(element.innerHTML);
-        element.style.display = 'flex';
-        console.log("here: ", element.innerHTML);
-      }
-    }
-  },
-  false
+    "DOMContentLoaded",
+    function () {
+        const addressElements = document.getElementsByClassName("address-content");
+        console.log("here: ", addressElements.length);
+        if (addressElements.length > 0) {
+            for (let index = 0; index < addressElements.length; index++) {
+                const element = addressElements[index];
+                element.innerHTML = formatAddress(element.innerHTML);
+                element.style.display = 'flex';
+                console.log("here: ", element.innerHTML);
+            }
+        }
+    },
+    false
 );
 
 function isJson(str) {
-  try {
-    JSON.parse(str);
-    return true;
-  } catch (e) {
-    return false;
-  }
+    try {
+        JSON.parse(str);
+        return true;
+    } catch (e) {
+        return false;
+    }
 }
 
 // Function to format the address string
 function formatAddress(address) {
-  const obj = JSON.parse(address);
-  let addressString = "";
+    const obj = JSON.parse(address);
+    let addressString = "";
 
-  if (obj.houseNumber) {
-    addressString += obj.houseNumber + ", ";
-  }
-  if (obj.streetName) {
-    addressString += obj.streetName + ", ";
-  }
-  if (obj.ward.name) {
-    addressString += obj.ward.name + ", ";
-  }
-  if (obj.district.name) {
-    addressString += obj.district.name + ", ";
-  }
-  if (obj.city.name) {
-    addressString += obj.city.name;
-  }
-  // Remove trailing comma and space if present
-  return addressString;
+    if (obj.houseNumber) {
+        addressString += obj.houseNumber + ", ";
+    }
+    if (obj.streetName) {
+        addressString += obj.streetName + ", ";
+    }
+    if (obj.ward.name) {
+        addressString += obj.ward.name + ", ";
+    }
+    if (obj.district.name) {
+        addressString += obj.district.name + ", ";
+    }
+    if (obj.city.name) {
+        addressString += obj.city.name;
+    }
+    // Remove trailing comma and space if present
+    return addressString;
 }
